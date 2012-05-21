08d88c1de46f6107d27cb834736c633cbd30e613
= <=>
2012-05-21 11:31:31 +0200

    first cut on the 4.1

--- 4.1.create-dedicated-bare-pr-svn-branch.groovy
+++ 4.1.create-dedicated-bare-pr-svn-branch.groovy
@@ -1,3 +1,37 @@
 // Groovy ant-script
-def ant = new AntBuilder();
-ant.echo( "start creating bare pr branch" )
+
+
+println( "SETUP: One bare pr branch" )
+
+class ScriptCommands {	
+	def ant = new AntBuilder();
+
+	def git(String gitWorkingDir, String command,String ... arguments) {		
+		println("GIT: " + command + " " + arguments );		
+		ant.exec(executable:"git",dir:gitWorkingDir,resultproperty:"cmdExit") {
+			arg(value:command)	
+			for( argument in arguments ) { 
+				arg(value:argument)
+			}
+		}
+		if( "0" != "${ant.project.properties.cmdExit}" ) throw new RuntimeException("Error executing ant command: " + command );
+		return this;						
+	}
+	
+	def create_dir( String parent, String dir ) {
+		ant.mkdir( dir: parent + "/" + dir )
+	}
+}
+
+def script = new ScriptCommands();
+
+def mydir = "E:/tmp/mult_bare/ant"
+
+script.git( mydir,"svn", "clone", "--prefix", "svn/kaksi/", "http://localhost/svn-repos/myrepo/branches/kaksi", "kaksi.git.gatekeeper", "--username", "adm");
+//script.git( mydir + "/kaksi.git.gatekeeper", "branch", "-m", "master", "kaksi");
+script.create_dir( mydir, "kaksi_bare" )
+script.git( mydir + "/kaksi_bare", "init", "--bare" )
+script.git( mydir + "/kaksi.git.gatekeeper","remote" ,"add", "bare_repo", "../kaksi_bare" )
+script.git( mydir + "/kaksi.git.gatekeeper", "push" ,"-u", "bare_repo", "master" )
+
+script.git( mydir, "clone", "kaksi_bare", "dev" )
